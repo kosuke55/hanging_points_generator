c4cb5860f88b797cf26f986cdbeb9a86c613dbce
kosuke55 <kosuke.tnp@gmail.com>
2020-03-20 00:27:56 +0900

    [icp] Use argparser

--- icp_camera_pose_estimation.py
+++ icp_camera_pose_estimation.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+import argparse
 import copy
 import numpy as np
 import open3d as o3d
@@ -14,10 +15,18 @@ def draw_registration_result_original_color(source, target, transformation):
     o3d.visualization.draw_geometries([source_temp, target])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--input', '-i', type=str,
+                        help='input dir',
+                        default='save_dir/')
+    args = parser.parse_args()
+
     width = 1920
     height = 1080
-    intrinsic_np = np.loadtxt("savedir/intrinsic.txt")
+    intrinsic_np = np.loadtxt(args.input + 'camera_pose/intrinsic.txt')
+
     intrinsic = o3d.camera.PinholeCameraIntrinsic()
     fx = intrinsic_np[0, 0]
     fy = intrinsic_np[1, 1]
@@ -32,15 +41,16 @@ if __name__ == "__main__":
     voxel_length = 0.002
     image_num = 24
 
-    print("Create point cloud from rgb and depth.")
+    print('Create point cloud from rgb and depth.')
     for i in range(image_num):
-        print("Create {:d}-th point cloud.".format(i))
-        camera_pose = np.loadtxt("savedir/camera_pose{:03}.txt".format(i))
+        print('Create {:d}-th point cloud.'.format(i))
+        camera_pose = np.loadtxt(
+            args.input + 'camera_pose/camera_pose{:03}.txt'.format(i))
 
         color = o3d.io.read_image(
-            "savedir/color{:03}.png".format(i))
+            args.input + 'color{:03}.png'.format(i))
         depth = o3d.io.read_image(
-            "savedir/depth{:03}.png".format(i))
+            args.input + 'depth{:03}.png'.format(i))
 
         rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
             color, depth, depth_trunc=4.0, convert_rgb_to_intensity=False)
@@ -66,12 +76,13 @@ if __name__ == "__main__":
         # mesh.create_from_point_cloud_poisson(pcd)
         # o3d.visualization.draw_geometries([mesh])
 
-    np.savetxt("savedir/camera_pose_icp000.txt", camera_poses[0].T())
+    # np.savetxt(args.input + 'camera_pose/camera_pose_icp000.txt',
+    #            camera_poses[0].T())
 
-    print("ICP registration start.")
+    print('ICP registration start.')
     target = pcds[0]
     for i in range(image_num-1):
-        print("ICP registration {:d}-th point cloud.".format(i))
+        print('ICP registration {:d}-th point cloud.'.format(i))
         trans_init = camera_poses[0].copy_worldcoords().inverse_transformation(
         ).transform(camera_poses[i+1])
 
@@ -80,9 +91,12 @@ if __name__ == "__main__":
         # draw_registration_result_original_color(source, target,
         #                                         trans_init.T())
 
+        # result_icp = o3d.registration.registration_icp(
+        #     source, target, 0.005, trans_init.T(),
+        #     o3d.registration.TransformationEstimationPointToPlane())
         result_icp = o3d.registration.registration_icp(
-            source, target, 0.02, trans_init.T(),
-            o3d.registration.TransformationEstimationPointToPlane())
+            source, target, 0.01, trans_init.T(),
+            o3d.registration.TransformationEstimationPointToPoint())
         # print(result_icp.transformation)
 
         icp_coords = skrobot.coordinates.Coordinates(
@@ -91,11 +105,12 @@ if __name__ == "__main__":
         camera_pose_icp = camera_poses[0].copy_worldcoords(
         ).transform(icp_coords)
 
-        np.savetxt("savedir/camera_pose_icp{:03}.txt".format(i+1),
-                   camera_pose_icp.T())
+        # np.savetxt(
+        #     args.input + 'camera_pose/camera_pose_icp{:03}.txt'.format(i+1),
+        #     camera_pose_icp.T())
 
         # Save camera pose and intrinsic for texture-mapping
-        with open('savedir/color{:03}.txt'.format(i), 'w') as f:
+        with open(args.input + 'color{:03}.txt'.format(i), 'w') as f:
             np.savetxt(f, np.concatenate(
                 [camera_pose_icp.T()[:3, 3][None, :],
                  camera_pose_icp.T()[:3, :3]],
@@ -118,4 +133,4 @@ if __name__ == "__main__":
     cl, ind = target.remove_radius_outlier(nb_points=100, radius=0.01)
     target = target.select_down_sample(ind)
     o3d.visualization.draw_geometries([target])
-    o3d.io.write_point_cloud("icp_result.ply", target)
+    o3d.io.write_point_cloud('icp_result.ply', target)
