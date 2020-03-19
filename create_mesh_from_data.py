d93efe1dc5a7b66b24cd175b2ecd9c4bc941f2a6
kosuke55 <kosuke.tnp@gmail.com>
2020-03-20 00:36:22 +0900

    [create_mesh_from_data.py] Use argparser

--- create_mesh_from_data.py
+++ create_mesh_from_data.py
@@ -1,36 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
 import open3d as o3d
 import numpy as np
+import trimesh
 
 if __name__ == "__main__":
-    intrinsic_np = np.load("create_mesh_sample_data/intrinsic.npy")
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--input', '-i', type=str,
+                        help='input dir',
+                        default='savedir/')
+    parser.add_argument('--output', '-o', type=str,
+                        help='output file name',
+                        default='save_dir/out.ply')
+    parser.add_argument('--num', '-n', type=int,
+                        help='How many images',
+                        default=24)
+    args = parser.parse_args()
+
+    intrinsic_np = np.loadtxt(args.input + "camera_pose/intrinsic.txt")
+    print(intrinsic_np)
     intrinsic = o3d.camera.PinholeCameraIntrinsic()
     intrinsic.set_intrinsics(
-        960,
-        540,
+        1920,
+        1080,
         intrinsic_np[0, 0],
         intrinsic_np[1, 1],
         intrinsic_np[0, 2],
         intrinsic_np[1, 2])
 
     volume = o3d.integration.ScalableTSDFVolume(
-        voxel_length=0.001,
-        sdf_trunc=0.01,
+        voxel_length=0.002,
+        sdf_trunc=0.005,
         color_type=o3d.integration.TSDFVolumeColorType.RGB8)
 
-    for i in range(8):
+    image_num = args.num
+    for i in range(image_num):
         print("Integrate {:d}-th image into the volume.".format(i))
-        camera_pose = np.load(
-            "create_mesh_sample_data/camera_pose{}.npy".format(i))
+
+        camera_pose = np.loadtxt(
+            args.input +
+            "camera_pose/camera_pose_icp{:03}.txt".format(i))
+        # camera_pose = np.load("savedir/camera_pose{:03}.npy".format(i))
         color = o3d.io.read_image(
-            "create_mesh_sample_data/color{}.png".format(i))
+            args.input + "/color{:03}.png".format(i))
         depth = o3d.io.read_image(
-            "create_mesh_sample_data/depth{}.png".format(i))
+            args.input + "/depth{:03}.png".format(i))
 
         rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
             color, depth, depth_trunc=4.0, convert_rgb_to_intensity=False)
+        # pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+        #     rgbd, intrinsic)
+        # o3d.visualization.draw_geometries([pcd])
+
         volume.integrate(
             rgbd,
             intrinsic,
@@ -40,3 +64,4 @@ if __name__ == "__main__":
     mesh = volume.extract_triangle_mesh()
     mesh.compute_vertex_normals()
     o3d.visualization.draw_geometries([mesh])
+    o3d.io.write_triangle_mesh(args.output, mesh)
