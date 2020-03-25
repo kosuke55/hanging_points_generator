55b8a5b28ead77878ced03fb5ea3f6efeef598df
kosuke55 <kosuke.tnp@gmail.com>
2020-03-26 02:54:28 +0900

    [check_contact_point_from_json] Update default file path

--- hanging_points_generator/check_contact_points_from_json.py
+++ hanging_points_generator/check_contact_points_from_json.py
@@ -6,15 +6,18 @@ import json
 import os
 import skrobot
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--input', '-i', type=str,
                     help='input contact points',
-                    default='contact_points.json')
+                    default=current_dir +
+                    '/../urdf/610/scissors/contact_points.json')
 parser.add_argument('--urdf', '-u', type=str,
                     help='input urdf',
-                    default='../urdf/610/scissors/base.urdf')
+                    default=current_dir
+                    + '/../urdf/610/scissors/base.urdf')
 args = parser.parse_args()
 
 contact_points_dict = json.load(open(args.input, 'r'))
