32826db2b6a5881021e8352bffe2ad755a7454ae
kosuke55 <kosuke.tnp@gmail.com>
2020-08-14 04:02:35 +0900

    Update random mesh hpg

--- random_mesh/generate_hanging_points.py
+++ random_mesh/generate_hanging_points.py
@@ -3,18 +3,19 @@
 
 import glob
 import os
+import os.path as osp
 
 from hanging_points_generator import hp_generator as hpg
 
-files = glob.glob("/media/kosuke55/SANDISK/meshdata/random_shape_ycb/*/*")
+urdf_dir = '/media/kosuke55/SANDISK/meshdata/random_shape_ycb'
+files = sorted(
+    glob.glob(osp.join(urdf_dir, '*/*')))
 
 for file in files:
     dirname, filename = os.path.split(file)
     filename_without_ext, ext = os.path.splitext(filename)
 
     if filename == 'base.urdf':
-        print('-----------------------')
-        print(file)
         hpg.generate(urdf_file=file,
                      required_points_num=30,
                      enable_gui="true",
