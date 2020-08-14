a4081886af50b738656c9d037d6481ef70ac18a7
kosuke55 <kosuke.tnp@gmail.com>
2020-08-14 17:14:16 +0900

    Update on3

--- Public-Datasets/ObjectNet3D/generate_hanging_points.py
+++ Public-Datasets/ObjectNet3D/generate_hanging_points.py
@@ -10,15 +10,29 @@ import os.path as osp
 urdf_dir = '/media/kosuke55/SANDISK/meshdata/Hanging-ObjectNet3D-DoubleFaces/CAD/urdf'
 files = glob.glob(osp.join(urdf_dir, "*/*/*"))
 
+hanging_object_dict = {
+    0: 'cap',
+    1: 'cup',
+    2: 'headphone',
+    3: 'helmet',
+    4: 'key',
+    5: 'scissors',
+    6: 'slipper'
+}
+
 for file in files:
     dirname, filename = os.path.split(file)
     filename_without_ext, ext = os.path.splitext(filename)
     category_name = dirname.split("/")[-2]
+
+    # if category_name != hanging_object_dict[5]:
+    #     continue
+
     if 'urdf' in ext.lower():
         print(file)
-        # hpg.generate(urdf_file=file,
-        #              required_points_num=5,
-        #              enable_gui="false",
-        #              viz_obj="false",
-        #              save_dir=dirname,
-        #              render=False)
+        hpg.generate(urdf_file=file,
+                     required_points_num=5,
+                     enable_gui="false",
+                     viz_obj="false",
+                     save_dir=dirname,
+                     render=False)
