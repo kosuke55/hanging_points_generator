f77b4f37bca6feaa6b7eaf558bbbeff729ca00af
kosuke55 <kosuke.tnp@gmail.com>
2020-09-04 03:59:09 +0900

    Update downloader

--- donwload_sample_data.py
+++ donwload_sample_data.py
@@ -3,13 +3,13 @@ import subprocess
 import gdown
 
 
-url = 'https://drive.google.com/uc?export=download&id=1Ky6rrfpPxGyzzVK6ZwAnA1U40aJwXfrH'
+url = 'https://drive.google.com/uc?export=download&id=1UGSOMWHXaQBmoJFXAd5NGefd_DdLjPna'
 
 output = 'sample_data.tgz'
 gdown.download(url, output, quiet=False)
 
-md5 = '1721569ed4e0d83ab1b65eb34dfcd546'
+md5 = 'ab9f0c690c79e1959742ec0dbfc162d3'
 
 gdown.cached_download(url, output, md5=md5, postprocess=gdown.extractall)
 
-subprocess.call('tar -xzvf sample_data.tgz sample_data', shell=True)
+subprocess.call('tar -xzvf sample_data.tgz', shell=True)
