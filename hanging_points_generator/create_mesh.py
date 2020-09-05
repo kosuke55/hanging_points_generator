0c45a35468b8799be739cbdbc832f88c5d5a39b0
kosuke55 <kosuke.tnp@gmail.com>
2020-09-05 17:48:05 +0900

    Modify import order

--- hanging_points_generator/create_mesh.py
+++ hanging_points_generator/create_mesh.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
-import open3d as o3d
 import os
 import pathlib2
+
+import numpy as np
+import open3d as o3d
 import skrobot
 import trimesh
 import xml.etree.ElementTree as ET
