1fca548dbc588b5c7e811aaf2b54e65d3b8758c6
kosuke55 <kosuke.tnp@gmail.com>
2020-09-08 06:26:04 +0900

    Update downloader

--- hanging_points_generator/donwloader.py
+++ hanging_points_generator/donwloader.py
@@ -1,12 +1,16 @@
-import shutil
+import os.path as osp
+
 import gdown
 
 
 def download_sample_data(output_dir=None):
-    url = 'https://drive.google.com/uc?export=download&id=1UGSOMWHXaQBmoJFXAd5NGefd_DdLjPna'  # noqa
-    output = 'sample_data.tgz'
-    md5 = 'ab9f0c690c79e1959742ec0dbfc162d3'
-    gdown.cached_download(url, output, md5=md5, postprocess=gdown.extractall)
-
-    if output_dir is not None:
-        shutil.move('sample_data', output_dir)
+    # color, depth, camera_pose
+    if output_dir is None:
+        output = 'sample_data.tgz'
+    else:
+        output = osp.join(output_dir, 'sample_data.tgz')
+    gdown.cached_download(
+        'https://drive.google.com/uc?export=download&id=1UGSOMWHXaQBmoJFXAd5NGefd_DdLjPna',
+        output,
+        md5='ab9f0c690c79e1959742ec0dbfc162d3',
+        postprocess=gdown.extractall)
