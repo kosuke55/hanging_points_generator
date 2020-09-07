b1daf6d92a8ba6f0806ab4dd7330e9e82028330a
kosuke55 <kosuke.tnp@gmail.com>
2020-09-08 06:17:12 +0900

    Fix donwloader

--- hanging_points_generator/donwloader.py
+++ hanging_points_generator/donwloader.py
@@ -1,16 +1,12 @@
-import subprocess
-
+import shutil
 import gdown
 
 
 def download_sample_data(output_dir=None):
     url = 'https://drive.google.com/uc?export=download&id=1UGSOMWHXaQBmoJFXAd5NGefd_DdLjPna'  # noqa
     output = 'sample_data.tgz'
-    gdown.download(url, output, quiet=False)
     md5 = 'ab9f0c690c79e1959742ec0dbfc162d3'
     gdown.cached_download(url, output, md5=md5, postprocess=gdown.extractall)
-    if output_dir is None:
-        subprocess.call('tar -xzvf sample_data.tgz', shell=True)
-    else:
-        subprocess.call('tar -xzvf sample_data.tgz -C {}'.format(
-            output_dir), shell=True)
+
+    if output_dir is not None:
+        shutil.move('sample_data', output_dir)
